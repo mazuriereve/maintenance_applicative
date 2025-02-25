6f4a8c733382bbd6e03e850d07ed09fee02cfdab
dupuis brian <dupuisbrian3@gmail.com>
2025-02-25 15:17:44 +0100

    connexion

--- connexion.php
+++ connexion.php
@@ -3,6 +3,7 @@ $host = "localhost"; // Serveur MySQL
 $dbname = "maintenance_applicative"; // Nom de la base de données
 $username = "root"; // Nom d'utilisateur MySQL
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
